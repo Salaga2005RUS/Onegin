e813d733eb7582f0cc423f6e3db622b60f9e65db
Alexander Paniman <alexpaniman@gmail.com>
2023-09-17 21:11:25 +0300

    Review code

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-COMPILER = g++
+COMPILER = g++ # TODO: Compilers are traditionally named $(CC) or $(CXX) in makefiles
 FLAGS = -g -ggdb3
-DOP_FLAGS = -Wall -Wextra
+DOP_FLAGS = -Wall -Wextra # TODO: What is DOP_FLAGS? Use a better name!
 ASSAN = -fsanitize=address
 
 FLAGS += $(DOP_FLAGS)
@@ -9,6 +9,8 @@ FLAGS += $(DOP_FLAGS)
 
 all: onegin
 
+# TODO: Don't .o depend on some headers? What will happend if you forget to include them?)
+#       (also, is it possible to somehow write this shorter?)
 function.o: functions.cpp
 	$(COMPILER) -c functions.cpp -o functions.o $(FLAGS)
 
@@ -17,3 +19,8 @@ main.o: main.cpp
 
 onegin: functions.o main.o
 	$(COMPILER)  functions.o main.o -o onegin.exe $(FLAGS)
+
+# TODO: Don't include binary files (e.g. .exe files), git wasn't designed to work with them,
+#       they make repositories slow, big, and it's almost impossible to rebase with them.
+
+#       Also, it's hard to sync them, and if you don't, what is the point?
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
-//main_file
+//main_file TODO: isn't this obvious?)
 
-#include "functions.h"
+#include "functions.h" // TODO: what is functions.h? This name is as good as no name at all.
+                       //
+	               //       It's highly obvious that most header files contain functions
+                       //       to anyone who has any knowledge about C programming language.
+                       //
+                       //       The main thing you should have told through your name is what
+                       //       kind of functions are inside of there. Rename it at once!
 
 #include <stdlib.h>
 
@@ -8,9 +14,12 @@ int main()
 {
     char input_file_name[] = "data.txt";
     char output_file_name[] = "data_sort.txt";
-    char *buf;
-    char **text;
+    char *buf; // TODO: always initialize your variables, it's much safer to have a predictable
+               //       value inside of your variables, not random junk.
+    char **text; // TODO: same thing applies everywhere
 
+    // TODO: You should create struct that will hold all this data (number of symbols, lines, ...),
+    //       instead of just passing it around chaotically. This of a good name too...
     int amount_of_cymbols = read_data(&buf, input_file_name);
 
     int n_counter = n_0_replacement(buf, amount_of_cymbols);
